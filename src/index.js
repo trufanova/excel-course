4e701def116655c338c9700d875e983d7d206095
Maria Trufanova <5014685@mail.ru>
2021-01-17 17:28:01 +0300

    finished setup enviroment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log('Hello, World!')
+import './scss/index.scss'
+
+console.log('Working!')
--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,57 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require("copy-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+
+const jsLoaders = () => {
+    const loaders = [{            
+        loader: "babel-loader",
+        options: {
+            presets: ['@babel/preset-env']
+        }  
+    } 
+    ]
+    if (isDev) {
+        loaders.push('eslint-loader')
+    }
+    return loaders
+}
+
 module.exports = {
     context: path.resolve(__dirname, 'src'), // здесь пишем папку с исхдниками, чтобы постоянно не писать к ней путь. Т.е. это как бы будет текущий контекст.
     mode: 'development', // мы устанавливаем режим по-умолчанию разработка, чтобы в консоли писать чуть меньше кода
-    entry: './index.js', //здесь указываем входные точки для приложения
+    target: process.env.NODE_ENV === "development" ? "web" : "browserslist",
+    entry: ['@babel/polyfill','./index.js'], //здесь указываем входные точки для приложения
     output: { //здесь пишем настройки для выходных файлов
-        filename: 'bundle.[hash].js', //Это имя файла, [hash] нужен, чтобы избежать проблем с кэшированием. Теперь к имени выходного файла будут добавляться цифры из хэша и он будет уникальным.
+        filename: filename('js'), //Это имя файла, [hash] нужен, чтобы избежать проблем с кэшированием. Теперь к имени выходного файла будут добавляться цифры из хэша и он будет уникальным.
         path: path.resolve(__dirname, 'dist') //метод path.resolve вернет нам абсолютный путь к выходному файлу
     },
     resolve: {
-        extentions: ['.js'],
+        extensions: ['.js'],
         alias: {
             '@': path.resolve(__dirname, 'src'),
             '@core': path.resolve(__dirname, 'src/core')
 
         }
     },
+    devtool: isDev ? 'source-map' : false,
+    devServer: {
+        port: 4200,
+        hot: isDev,
+        overlay: true,
+        open: true
+    },
     plugins: [
         new CleanWebpackPlugin(),
         new HTMLWebpackPlugin({
-            template: 'index.html'
+            template: 'index.html',
+            minify: {
+                removeComments: isProd,
+                collapseWhitespace: isProd
+            }
         }),
         new CopyPlugin({
             patterns: [
@@ -33,7 +64,35 @@ module.exports = {
             ],
           }),
           new MiniCssExtractPlugin({
-              filename: 'bundle.[hash].css'
+              filename: filename('css')
           })
-    ]
+    ],
+    module: {
+        rules: [
+             {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: (resourcePath, context) => {
+                                return path.relative(path.dirname(resourcePath), context) + '/';
+                            },
+                            // hmr: isDev,
+                            // reloadAll: true
+                        },
+                    },
+                    "css-loader",
+                    "sass-loader",
+                ],
+            }, 
+
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: jsLoaders(),
+
+            }
+        ],
+    }
 }
